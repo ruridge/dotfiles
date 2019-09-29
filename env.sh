b9dcc2058f2c0b678d0da90df211baff1737fbf2
Rob Uridge <rob@uridge.com>
2019-09-29 01:57:17 +0100

    add env

---
+++ env.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+# Add commonly used folders to $PATH
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+# Specify default editor. Possible values: vim, nano, ed etc.
+export EDITOR=vim
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# File search functions
+function f() { find . -iname "*$1*" ${@:2} }
+function r() { grep "$1" ${@:2} -R . }
+
+# Create a folder and move into it in one command
+function mkcd() { mkdir -p "$@" && cd "$_"; }
+
+# Aliases
+alias g='git'
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
